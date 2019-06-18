33c6fffb8d36577f233d76c7dedd01aa2f584aab
Preston Whitehall <pmwhitehall@gmail.com>
2019-06-18 10:54:44 -0700

    updates to cardDetails object and detectNetwork.js

--- detectNetwork.js
+++ detectNetwork.js
@@ -7,7 +7,6 @@
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
 
-var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
@@ -17,35 +16,6 @@ var detectNetwork = function(cardNumber) {
 // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
 // Discover always has a prefix of 6011, 644-649, or 65, and a length of 16 or 19.
 // Maestro always has a prefix of 5018, 5020, 5038, or 6304, and a length of 12-19.
-
-    let cardDetails = {
-      'Visa' : {
-        'prefix' : ['4'],
-        'cardLength' : [13, 16, 19]
-      },
-      'MasterCard': {
-        'prefix' :['51', '52', '53', '54', '55'],
-        'cardLength' : [16]
-      },
-      'Discover' : {
-        'prefix' : ['6011', '644', '645', '646', '647', '648', '649', '65'],
-        'cardLength' : [16, 19]
-      },
-      'Maestro' : {
-        'prefix' : ['5018', '5020', '5038', '6304'],
-        'cardLength' : [12, 13, 14, 15, 16, 17, 18, 19]
-      },
-      'Diner\'s Club' : {
-        'prefix' : ['38', '39'],
-        'cardLength' : [14]
-      },
-      'American Express' : {
-        'prefix' : ['34', '37'],
-        'cardLength' : [15]
-      }
-
-    }
-
     let populateDetails = function(company, keyName, start, end) {
       for (let i = start; i <= end; i++) {
         cardDetails[company][keyName].push(i.toString());
@@ -58,17 +28,93 @@ var detectNetwork = function(cardNumber) {
       return card;
     }
 
-    let prefix = '';
-    let cardLength = cardNumber.length;
+    let cardDetails = {
+      'Visa' : {
+        'prefix' : [[4]],
+        'cardLength' : [[13], [16], [19]]
+      },
+      'MasterCard': {
+        'prefix' :[[51, 55]],
+        'cardLength' : [[16]]
+      },
+      'Discover' : {
+        'prefix' : [[6011], [644, 649], [65]],
+        'cardLength' : [[16], [19]]
+      },
+      'Maestro' : {
+        'prefix' : [[5018], [5020], [5038], [6304]],
+        'cardLength' : [[12, 19]]
+      },
+      'Diner\'s Club' : {
+        'prefix' : [[38], [39]],
+        'cardLength' : [[14]]
+      },
+      'American Express' : {
+        'prefix' : [[34], [37]],
+        'cardLength' : [[15]]
+      },
+      'China UnionPay' : {
+        'prefix' : [[622126, 622925], [624, 626], [6282, 6288]],
+        'length' : [[16, 19]]
+      },
+      'Switch' : {
+        'prefix' : [[4903], [4905], [4911], [4936], [564182], [633110], [6333], [6759]],
+        'length' : [[16], [18, 19]]
+      }
+    }
 
-    for (let i = 0; i < 5; i++) {
-      prefix += cardNumber[i];
-      for (let keys in cardDetails) {
-        if (cardDetails[keys]['prefix'].indexOf(prefix) > -1) {
-          if (cardDetails[keys]['cardLength'].indexOf(cardLength) > -1) {
-            return keys;
+var detectNetwork = function(cardNumber) {
+
+  let prefix = '';
+  let cardLength = cardNumber.length;
+  let checkCardLength = false;
+
+  for (let i = 0; i < 5; i++) {
+    prefix += cardNumber[i];
+    prefix = parseInt(prefix);
+
+    for (let keys in cardDetails) {
+      checkCardLength = false;
+      let cardIndex = cardDetails[keys]['prefix'];
+
+      // "MasterCard"
+      for (let i = 0; i < cardIndex.length; i++) {
+        let start = cardIndex[i][0];
+        let stop = cardIndex[i][0];
+
+        if (checkCardLength === true) {
+          break;
+        }
+
+        if (cardIndex[i].length !== 1) {
+          stop = cardIndex[i][1];
+        }
+
+        for (start; start <= stop; start++) {
+          if (start === prefix) {
+            checkCardLength = true;
+            break;
+          }
+        }
+      }
+
+      if (checkCardLength) {
+        let lengthIndex = cardDetails[keys]['cardLength'];
+        for (let i = 0; i < lengthIndex.length; i++) {
+          let start = lengthIndex[i][0];
+          let stop = lengthIndex[i][0];
+
+          if (lengthIndex[i].length !== 1) {
+            stop = lengthIndex[i][1];
+          }
+
+          for (start; start <= stop; start++) {
+            if (start === cardLength) {
+              return keys;
+            }
           }
         }
       }
     }
+  }
 };
